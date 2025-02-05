7383277016bc4dbf3df5044722beb27534497414
Prajwal Verma <verma.praj@northeastern.edu>
2025-02-05 12:01:09 -0500

    Dog Card Prepared

---
+++ src/Home_Screen/home_screen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+    Card,
+    CardMedia,
+    CardContent,
+    Table,
+    TableRow,
+    TableCell,
+    Button
+} from '@mui/material';
+import wallpaperImg from './wallpaper.jpg';
+
+import {
+    Favorite as FavoriteIcon
+ } from '@mui/icons-material';
+
+
+const HomeScreen = ( props ) => {
+    return (
+        <React.Fragment>
+            <Card sx={{ maxWidth: 345, borderRadius: "15px" }}>
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image={wallpaperImg}
+                    alt="Paella dish"
+                /> 
+                <CardContent>
+                    <Table>
+                        <TableRow>
+                            <TableCell>Name: Prajwal</TableCell>
+                            <TableCell align="right">Age: 26</TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell>Breed: Lion</TableCell>
+                            <TableCell align="right">Zip: 02135</TableCell>
+                        </TableRow>
+                    </Table>
+                    <br/>
+
+                    <Button
+                        color = "secondary"
+                        fullWidth
+                        startIcon = {<FavoriteIcon/>}
+                    >
+                        Add to Favorites
+                    </Button>
+                </CardContent>
+                
+            </Card>
+        </React.Fragment>
+    )
+}
+
+export default HomeScreen;
--- src/Login_Screen/login_screen.js
+++ src/Login_Screen/login_screen.js
@@ -5,67 +5,74 @@ import {
     TextField,
     Button
 } from '@mui/material';
-import wallpaperImg from './wallpaper.jpg'
+import wallpaperImg from './wallpaper.jpg';
+import HomeScreen from "../Home_Screen/Home_Screen";
 
 const Login_Screen = (props) => {
     const [ name, setName] = React.useState("");
     const [ email, setEmail] = React.useState("");
+    const [ showHome, setShowHome] = React.useState(false);
 
     const handleLogin = (event) => {
         event.preventDefault();
+        setShowHome( true );
     };
 
     return (
         <React.Fragment>
-            <Grid
-                container
-                className = "fullHeight"
-                >
-                <Grid item size={{ md:8 }}>
-                    <img 
-                        src={wallpaperImg} 
-                        alt="Sorry, No image found!" 
-                        className = "fullHeight centerFullWidth"
-                    />
-                </Grid>
-                <Grid item size={{ md:4 }}>
-                    <Grid container direction="column" spacing={2}>
-                        <Grid item size={{ md:12 }} className = "loginCard_1"></Grid>
-                        <Grid item size={{ md:12 }} className = "loginCard_3">
-                            <Typography  variant="h2" className = "headingClass">Shelter App</Typography>
+            {showHome?(
+                <HomeScreen/>
+            ):(
+                <Grid
+                    container
+                    className = "fullHeight"
+                    >
+                    <Grid item size={{ md:8 }}>
+                        <img 
+                            src={wallpaperImg} 
+                            alt="Sorry, No image found!" 
+                            className = "fullHeight centerFullWidth"
+                        />
+                    </Grid>
+                    <Grid item size={{ md:4 }}>
+                        <Grid container direction="column" spacing={2}>
+                            <Grid item size={{ md:12 }} className = "loginCard_1"></Grid>
+                            <Grid item size={{ md:12 }} className = "loginCard_3">
+                                <Typography  variant="h2" className = "headingClass">Shelter App</Typography>
+                            </Grid>
+                            <Grid item size={{ md:12 }} className = "loginCard_1">
+                                <TextField 
+                                    label="Name"
+                                    value = {name}
+                                    variant="outlined" 
+                                    onChange={(evt)=>setName(evt.target.value)}
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item size={{ md:12 }} className = "loginCard_1">
+                                <TextField 
+                                    label="Email" 
+                                    value = {email}
+                                    variant="outlined" 
+                                    onChange={(evt)=>setEmail(evt.target.value)}
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item size={{ md:12 }} className = "loginCard_1">
+                                <Button 
+                                    variant="contained" 
+                                    onClick={handleLogin}
+                                    fullWidth
+                                    size="large"
+                                > 
+                                    Submit 
+                                </Button>
+                            </Grid>
+                            <Grid item size={{ md:12 }} className = "loginCard_3"></Grid>
                         </Grid>
-                        <Grid item size={{ md:12 }} className = "loginCard_1">
-                            <TextField 
-                                label="Name"
-                                value = {name}
-                                variant="outlined" 
-                                onChange={(evt)=>setName(evt.target.value)}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item size={{ md:12 }} className = "loginCard_1">
-                            <TextField 
-                                label="Email" 
-                                value = {email}
-                                variant="outlined" 
-                                onChange={(evt)=>setEmail(evt.target.value)}
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item size={{ md:12 }} className = "loginCard_1">
-                            <Button 
-                                variant="contained" 
-                                onClick={handleLogin}
-                                fullWidth
-                                size="large"
-                            > 
-                                Submit 
-                            </Button>
-                        </Grid>
-                        <Grid item size={{ md:12 }} className = "loginCard_3"></Grid>
                     </Grid>
                 </Grid>
-            </Grid>
+            )}
         </React.Fragment>
     );
 }
